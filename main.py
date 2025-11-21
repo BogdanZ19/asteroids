a7232c517fa28a5a0cd77abd9017fea0f5397e80
BogdanZ19 <bogdanzainia1@gmail.com>
2025-11-21 23:04:31 +0200

    add forward/backward movement and refactored using groups

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
@@ -21,9 +26,13 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
     
         
